75f1efff666a65ce325a2e76111cc7f6ae894a4c
kupbanana <81219232+kupbanana@users.noreply.github.com>
2022-10-04 11:24:53 +0200

    poprawa

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,4 @@
-go.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from zamowienia.models import Zamowienie
 from django.urls import reverse

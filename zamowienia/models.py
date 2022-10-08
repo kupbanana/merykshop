4d37d6d8f7f0f1c53076c3e0453519d78d27409f
Ewelina Meryk <meryk154@gmail.com>
2022-10-08 11:30:49 +0200

    Zmiany w module administracyjnym zamowień

--- zamowienia/models.py
+++ zamowienia/models.py
@@ -8,8 +8,8 @@ class Zamowienie(models.Model):
     uzytkownik = models.ForeignKey(User,
                               related_name='uzytkownik',
                               on_delete=models.CASCADE)
-    #imie = models.CharField(max_length=50)
-    #nazwisko = models.CharField(max_length=50)
+    imie = models.CharField(max_length=50)
+    nazwisko = models.CharField(max_length=50)
     email = models.EmailField(default=None)
     adres = models.CharField(default='', max_length=250)
     kod_pocztowy = models.CharField(default='', max_length=20)
@@ -17,6 +17,7 @@ class Zamowienie(models.Model):
     utworzone = models.DateTimeField(auto_now_add=True)
     zaktualizowane = models.DateTimeField(auto_now=True)
     zaplacone = models.BooleanField(default=False)
+    gosc = models.BooleanField(default=True)
     id_serwisu_platniczego = models.CharField(max_length=150, blank=True)
 
     class Meta:
@@ -40,7 +41,7 @@ class Zamowienie(models.Model):
         return s
 
     def oblicz_laczna_kwote(self):
-        return sum(item.get_cost() for item in self.artykuly.all())
+        return sum(item.oblicz_cene() for item in self.artykuly.all())
 
 
 class PozycjaZamowienia(models.Model):
@@ -56,5 +57,5 @@ class PozycjaZamowienia(models.Model):
     def __str__(self):
         return str(self.id)
 
-    def get_cost(self):
+    def oblicz_cene(self):
         return self.cena * self.ilosc

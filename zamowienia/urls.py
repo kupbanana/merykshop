00c7049191239c2c761b55f0c258a23858806d22
kupbanana <81219232+kupbanana@users.noreply.github.com>
2022-10-08 14:27:41 +0200

    poprawki

--- zamowienia/urls.py
+++ zamowienia/urls.py
@@ -5,7 +5,7 @@ app_name = 'zamowienia'
 
 urlpatterns = [
     path('utworz/', views.utworz_zamowienie, name='utworz_zamowienie'),
-    path('admin/zamowienie/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-    path('admin/zamowienie/<int:order_id>/pdf/', views.admin_order_pdf, name='admin_order_pdf'),
+    path('admin/zamowienie/<int:id_zamowienia>/', views.admin_order_detail, name='admin_order_detail'),
+    path('admin/zamowienie/<int:id_zamowienia>/pdf/', views.admin_order_pdf, name='admin_order_pdf'),
     
 ]
